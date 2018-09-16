f145ce9c303652c60fc3640e1b42be867f3acdf7
wecanfuture <wecanfuture@gmail.com>
2018-09-16 15:05:07 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,4 +2,4 @@ def get_ebaylogin_url():
     return 'https://signin.ebay.de/ws/eBayISAPI.dll?SignIn'
 
 def get_ebaylogin_url_rate():
-    return 'https://www.ebay.de/sh/ord/?filter=status:ALL_ORDERS'
+    return 'https://my.ebay.de/ws/eBayISAPI.dll?MyEbayBeta&CurrentPage=MyeBayNextSold&ssPageName=STRK:ME:LNLK:MESOX'
